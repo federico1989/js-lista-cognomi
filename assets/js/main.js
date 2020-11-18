c58667c0503d1055adac4fdd4c23771d070e111e
Federico <federico.scarpati1989@gmail.com>
2020-11-18 17:24:39 +0100

    Esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
+// Lista Cognomi
+// Chiedere all'utente il cognome
+var writeSurname = prompt("Inserisci il tuo cognome");
+// Inserirlo in un array con altri cognomi: ‘Bianchi', ‘Rossi', ‘Duzioni', ‘Balsano', ‘Verdi'
+var listSurname = [
+  "Bianchi",
+  "Rossi",
+  "Duzioni",
+  "Balsano",
+  "Verdi"
+];
 
+listSurname.push(writeSurname);
+var orderSurname = listSurname.sort();
+console.log(listSurname);
+// Stampa la lista ordinata alfabeticamente
+for (var i = 0; i < orderSurname.length; i++) {
+  var item = orderSurname[i];
+  var contenutoLista = document.getElementById("lista-completa").innerHTML;
+  document.getElementById("lista-completa").innerHTML = contenutoLista + "<li>" + item + "</li>";
+}
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+document.getElementById("posizione").innerHTML = "Il tuo cognome è alla posizione numero: " + (orderSurname.indexOf(writeSurname)+1);
